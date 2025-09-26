bfca5f07043195f69c67501879fbdd77f29bd869
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-26 03:27:21 +0200

    feat(api): create database user entity with salted credentials

---
+++ pulsevote-backend/models/User.js
@@ -0,0 +1,34 @@
+﻿const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+// --- Internal
+
+const schema = new mongoose.Schema({
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    }
+});
+
+schema.pre("save", async function (next) {
+    if (!this.isModified("password")) {
+        return next();
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+});
+
+schema.methods.comparePassword = function (candidate) {
+    return bcrypt.compare(candidate, this.password);
+}
+
+// --- Exported
+
+module.exports = mongoose.model('User', schema);
