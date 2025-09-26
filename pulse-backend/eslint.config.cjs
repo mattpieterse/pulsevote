74e4a1fed8a7cba07a8e55ab08c1039e8b72ee39
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-26 23:02:56 +0200

    feat(api): fix dependency issues

---
+++ pulse-backend/eslint.config.cjs
@@ -0,0 +1,36 @@
+﻿// eslint.config.js
+const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+    {
+        ignores: ["node_modules/", "coverage/", "dist/", "build/", ".circleci/", "ssl/"]
+    },
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2023,
+            sourceType: "commonjs",
+            globals: {
+                ...globals.node,
+                ...globals.es2021,
+                process: "readonly",
+                __dirname: "readonly",
+                __filename: "readonly"
+            }
+        },
+        rules: {
+            ...js.configs.recommended.rules,
+            "no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
+            "no-console": "off"
+        }
+    },
+    {
+        files: ["test/**/*.test.js", "**/__tests__/**/*.js"],
+        languageOptions: {
+            globals: {
+                ...globals.jest
+            }
+        }
+    }
+];
