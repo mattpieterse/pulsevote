886ef358dfae32df2e197222e1e88e10beaa71e3
mattpieterse <63480247+mattpieterse@users.noreply.github.com>
2025-09-26 22:53:56 +0200

    feat(api): create eslint configuration

---
+++ eslint.config.cjs
@@ -0,0 +1,27 @@
+﻿const js = require("@eslint/js");
+const globals = require("globals");
+
+module.exports = [
+    {ignores: ["node_modules/", "coverage/", "dist/", "build/", ".circleci/", "ssl/"]},
+
+    js.configs.recommended,
+
+    {
+        files: ["**/*.js"],
+        languageOptions: {
+            ecmaVersion: 2023,
+            sourceType: "commonjs",
+            globals: {...globals.node, ...globals.es2021}
+        },
+        rules: {
+            "no-unused-vars": ["warn", {argsIgnorePattern: "^_"}]
+        }
+    },
+
+    {
+        files: ["test/**/*.test.js", "**/__tests__/**/*.js"],
+        languageOptions: {
+            globals: {...globals.jest}
+        }
+    }
+];
